dfa458036089626a751ecd9d5f9ba9a98f44dbd1
shreya kumari <shreya.kumari@rungta.org>
2026-01-08 12:32:16 +0530

    Add files via upload

---
+++ C_Project/cal.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+float add(float a, float b);
+float subtract(float a, float b);
+float multiply(float a, float b);
+float divide(float a, float b);
+
+int main()
+{
+    float num1, num2;
+    int choice;
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    printf("\nMini Calculator\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch(choice)
+    {
+        case 1:
+            printf("Result = %.2f", add(num1, num2));
+            break;
+
+        case 2:
+            printf("Result = %.2f", subtract(num1, num2));
+            break;
+
+        case 3:
+            printf("Result = %.2f", multiply(num1, num2));
+            break;
+
+        case 4:
+            if(num2 != 0)
+                printf("Result = %.2f", divide(num1, num2));
+            else
+                printf("Division by zero is not allowed");
+            break;
+
+        default:
+            printf("Invalid choice");
+    }
+
+    return 0;
+}
+
+
+float add(float a, float b)
+{
+    return a + b;
+}
+
+float subtract(float a, float b)
+{
+    return a - b;
+}
+
+float multiply(float a, float b)
+{
+    return a * b;
+}
+
+float divide(float a, float b)
+{
+    return a / b;
+}
